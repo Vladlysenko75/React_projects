b62aeb79d8765a088b8f5f69e5b1394ee8180d86
Vladlysenko75 <vladyslavlysenko75@gmail.com>
2022-01-13 19:45:47 +0200

    HW2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import './App.css';
-import {Users} from "./components/Users";
-import {Posts} from "./components/Posts";
-import {Comments} from "./components/Comments";
 
-export default function App() {
+function App() {
+  return (
+    <div className="App">
 
-    return (
-        <div className="App">
-
-            <div className="userPosts">
-                <div className="usersy">
-                    <Users/>
-                </div>
-                <div className="posts">
-                    <Posts/>
-                </div>
-            </div>
-            <div className="comments">
-                <Comments/>
-            </div>
-        </div>
-    );
+    </div>
+  );
 }
 
+export default App;
