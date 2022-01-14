d67d07c0ad0ef59e02d8ac1eaf613e6d64246b69
Vladlysenko75 <vladyslavlysenko75@gmail.com>
2022-01-14 22:24:40 +0200

    HW2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import './App.css';
 
+import {Inputs} from "./Components/Inputs";
+import {Users} from "./Components/Users";
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="inputs">
+        <Inputs/>
+      </div>
+      <div className="users">
+        <Users/>
+      </div>
     </div>
   );
 }
