379d1a89f16e2604c73a1b0aa8733542801c3efb
Vladlysenko75 <vladyslavlysenko75@gmail.com>
2022-01-19 15:35:10 +0200

    default

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import './App.css';
 
-import {Inputs} from "./Components/Inputs";
-import {Users} from "./Components/Users";
-
 function App() {
   return (
     <div className="App">
-      <div className="inputs">
-        <Inputs/>
-      </div>
-      <div className="users">
-        <Users/>
-      </div>
+
     </div>
   );
 }
