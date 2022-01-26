4944f56b192d9df12196a425246f922060e63142
Vladlysenko75 <vladyslavlysenko75@gmail.com>
2022-01-26 22:17:02 +0200

    ch

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ import App from './App';
 
 
 ReactDOM.render(
-  <React.StrictMode>
-      <BrowserRouter>
-          <App />
-      </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
