3443cda660bf82ffebf26b455d34f8589aa186b1
Vladlysenko75 <vladyslavlysenko75@gmail.com>
2022-01-27 20:53:00 +0200

    ch

--- src/App.js
+++ src/App.js
@@ -11,12 +11,11 @@ const reducer = (state, action) => {
             return {count1: state.count1 - 1}
         case 'reset':
             return {count1: 0}
-        case 'cat':
+        case 'cat': console.log(state)
             return
         default:
             throw new Error('Error')
     }
-    return state
 }
 
 function App() {
@@ -27,7 +26,7 @@ function App() {
             <div className="inputs">
                 <form>
                     <label>Cats: <input type="text"/></label>
-                    <button onClick={() => dispatch({type: 'cat'})}>Send Cats</button>
+                    <button onClick={() => dispatch({type: 'cat'}, 'sfgdfgdfg')}>Send Cats</button>
                     <label>Dogs: <input type="text"/></label>
                     <button>Send Dogs</button>
                 </form>
