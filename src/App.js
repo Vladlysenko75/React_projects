4bc490d0a2ff6a80f7f98ad9c69fbae1b5e9cdf9
Vladlysenko75 <vladyslavlysenko75@gmail.com>
2022-01-27 20:49:19 +0200

    ch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import {useReducer} from "react";
 
+
+
 const reducer = (state, action) => {
     switch (action.type) {
         case 'inc':
@@ -9,6 +11,8 @@ const reducer = (state, action) => {
             return {count1: state.count1 - 1}
         case 'reset':
             return {count1: 0}
+        case 'cat':
+            return
         default:
             throw new Error('Error')
     }
@@ -16,14 +20,23 @@ const reducer = (state, action) => {
 }
 
 function App() {
-    const [state, dispatch] = useReducer(reducer, {count1: 0});
+    const [state, dispatch] = useReducer(reducer, {count1: 0, cats: [], dogs: []});
 
     return (
         <div className="App">
+            <div className="inputs">
+                <form>
+                    <label>Cats: <input type="text"/></label>
+                    <button onClick={() => dispatch({type: 'cat'})}>Send Cats</button>
+                    <label>Dogs: <input type="text"/></label>
+                    <button>Send Dogs</button>
+                </form>
+
+            </div>
             <div>{state.count1}</div>
-            <button onClick={() => dispatch({type:'inc'})}>Increment</button>
-            <button onClick={() => dispatch({type:'dec'})}>Decrement</button>
-            <button onClick={() => dispatch({type:'reset'})}>RESET</button>
+            <button onClick={() => dispatch({type: 'inc'})}>Increment</button>
+            <button onClick={() => dispatch({type: 'dec'})}>Decrement</button>
+            <button onClick={() => dispatch({type: 'reset'})}>RESET</button>
         </div>
     );
 }
