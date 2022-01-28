4794282517ebcbe5e385823e8f4be0bdc8529330
Vladlysenko75 <vladyslavlysenko75@gmail.com>
2022-01-28 14:37:02 +0200

    Car form

--- src/components/Forms/Form.js
+++ src/components/Forms/Form.js
@@ -16,49 +16,49 @@ export const Form = ({setCarArray}) => {
     }
 
     const deleteCarByID = (car) => {
-        setCarArray(car)
-        console.log(car)
-        carService.deleteById(car.carID).catch(error => {
+        carService.deleteById(car.carID)
+            .then(() => setCarArray(car))
+            .catch(error => {
             setFormError(error.response.data)
         })
     }
 
     const updateCarInfo = (car) => {
-        setCarArray(car)
-        console.log(car)
-        carService.updateById(car.carID, car).catch(error => {
+        carService.updateById(car.carID, car)
+            .then(() => setCarArray(car))
+            .catch(error => {
             setFormError(error.response.data)
         })
     }
+
     return (
         <div>
             <form>
-                <div className="add">
-                    <h2>Add car:</h2>
+                <div className="inputs">
+                    <h2>Car inputs:</h2>
                     <label>Model: <input type="text" defaultValue={''} {...register('model')}/></label>
                     {formError.model && <span>{formError.model[0]}</span>}
                     <label>Price: <input type="number" defaultValue={''} {...register('price')}/></label>
                     {formError.price && <span>{formError.price[0]}</span>}
                     <label>Year: <input type="number" defaultValue={''} {...register('year')}/></label>
                     {formError.year && <span>{formError.year[0]}</span>}
-                    <button onClick={handleSubmit(submitNewCar)}>Save Car</button>
-                </div>
-                <div className="delete">
-                    <h2>Delete Car:</h2>
                     <label>Car ID:<input type="number"  {...register('carID')}/></label>
                     {formError.detail && <span>{formError.detail}</span>}
-                    <button onClick={handleSubmit(deleteCarByID)}>Delete Car</button>
                 </div>
-                <div className="update">
-                    <h2>Update Car:</h2>
-                    <label>Model: <input type="text" defaultValue={''} {...register('model')}/></label>
-                    {formError.model && <span>{formError.model[0]}</span>}
-                    <label>Price: <input type="number" defaultValue={''} {...register('price')}/></label>
-                    {formError.price && <span>{formError.price[0]}</span>}
-                    <label>Year: <input type="number" defaultValue={''} {...register('year')}/></label>
-                    {formError.year && <span>{formError.year[0]}</span>}
-                    <label>Car ID:<input type="number"  {...register('carID')}/></label>
-                    <button onClick={handleSubmit(updateCarInfo)}>Update Car</button>
+                <div className="buttons">
+                    <h3>Car actions:</h3>
+                    <div className="save">
+                        <p>Note: to add new car fulfill inputs(Model, Price, Year) and press Save Car button.</p>
+                        <button onClick={handleSubmit(submitNewCar)}>Save Car</button>
+                    </div>
+                    <div className="delete">
+                        <p>Note: to delete car enter car ID and press Delete Car button or simply press Delete button in the cars list below.</p>
+                        <button onClick={handleSubmit(deleteCarByID)}>Delete Car</button>
+                    </div>
+                    <div className="update">
+                        <p>Note: to update car info enter car ID, which you want to update and fulfill inputs(Model, Price, Year) with new information and press Update button.</p>
+                        <button onClick={handleSubmit(updateCarInfo)}>Update Car</button>
+                    </div>
                 </div>
             </form>
         </div>
