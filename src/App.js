f524d5c4a8754e1cfd2511de6e385a2fe643306e
Vladlysenko75 <vladyslavlysenko75@gmail.com>
2022-02-01 19:55:05 +0200

    main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import './App.css';
-import {useReducer} from "react";
-
-const reducer = (state, action) => {
-    switch (action.type) {
-        case 'inc':
-            return
-    }
-    return state
-}
 
 function App() {
-    const [state, dispatch] = useReducer(reducer, {count1: 0});
 
     return (
         <div className="App">
-            <div>{state.count1}</div>
-            <button onClick={() => dispatch({type:'inc'})}>Increment</button>
-            <button onClick={() => dispatch({type:'dec'})}>Decrement</button>
-            <button onClick={() => dispatch({type:'reset'})}>RESET</button>
+
         </div>
     );
 }
